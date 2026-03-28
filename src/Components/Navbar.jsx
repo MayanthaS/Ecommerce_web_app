e8b9ef7c6c81784aaec61fba62fe1a94cda56f55
MayanthaS <sapumalmayantha@gmail.com>
2026-03-28 23:29:21 +0530

    feat: add location display logic in Navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const location = null; // Replace with actual location logic
   return (
     <div className="bg-white py-3 shadow-2xl px-4 ">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
@@ -15,6 +16,9 @@ const Navbar = () => {
           </Link>
           <div className="flex gap-2 text-gray-800 items-center">
             <MapPin className="text-blue-700" />
+            <span className="font-semibold">
+              {location ? <div></div> : "Add Location"}
+            </span>
           </div>
         </div>
       </div>
