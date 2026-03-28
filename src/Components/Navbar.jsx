2fcd777b63b2784ca7c3601044a0cdc44f51dbb4
MayanthaS <sapumalmayantha@gmail.com>
2026-03-28 23:34:35 +0530

    feat: refactor Navbar component layout and integrate menu section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { MapPin } from "lucide-react";
 import React from "react";
+import { FaCaretDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -8,7 +9,7 @@ const Navbar = () => {
     <div className="bg-white py-3 shadow-2xl px-4 ">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         {/* Logo */}
-        <div className="">
+        <div className="flex items-center gap-4">
           <Link to="/">
             <h1 className="font-bold text-4xl ">
               <span className="text-blue-700">My</span>Shop
@@ -19,8 +20,18 @@ const Navbar = () => {
             <span className="font-semibold">
               {location ? <div></div> : "Add Location"}
             </span>
+            <FaCaretDown />
           </div>
         </div>
+        {/* Menu Section */}
+        <nav>
+          <ul className="flex gap-7 items-center text-xl font-semibold">
+            <li>Home</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
