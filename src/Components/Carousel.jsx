5dc43fd8c61696bf9b112905b797a4bb3e6bcbb7
MayanthaS <sapumalmayantha@gmail.com>
2026-03-30 12:54:11 +0530

    feat: update Carousel component with improved item layout and styling

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -29,19 +29,34 @@ const Carousel = () => {
           return (
             <div
               key={index}
-              className="bg-linear-to-r from-[#0f0c29] via-[#302b63] to-[#24243e  ]-z-10 "
+              className="bg-linear-to-r from-[#140b66] via-[#2c2769] to-[#24243e  ]-z-10 "
             >
               <div className="flex gap-10 justify-center h-96 items-center px-4">
                 <div className="space-y-6">
-                  <h3>abacd</h3>
+                  <h3 className="text-white font-semibold font-sans text-sm ">
+                    Powering Your World With Best Items{" "}
+                  </h3>
+                  <h1 className="text-4xl font-bold  text-blue-700 line-clamp-2 w-[500px] ">
+                    {item.title}
+                  </h1>
+                  <p className="text-gray-300 md:w-[500px] pr-2">
+                    {item.description}
+                  </p>
+                  <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mb-2 mt-2">
+                    Shop Now
+                  </button>
+                </div>
+                <div className="flex-shrink-0">
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="w-64 h-64 object-contain"
+                  />
                 </div>
               </div>
             </div>
           );
         })}
-        <div>
-          <h3>1</h3>
-        </div>
       </SlickSlider>
     </div>
   );
