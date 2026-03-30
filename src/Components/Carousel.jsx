a45052e9028c4e49e6a2fca7b7926015d8f9482b
MayanthaS <sapumalmayantha@gmail.com>
2026-03-30 12:07:26 +0530

    feat: implement Carousel component with product data integration and slider functionality

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,5 +1,8 @@
 import { useContext, useEffect } from "react";
 import { DataContext } from "../context/DataContext";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Carousel = () => {
   const { data, fetchAllProducts } = useContext(DataContext);
@@ -8,8 +11,38 @@ const Carousel = () => {
   useEffect(() => {
     fetchAllProducts();
   }, [fetchAllProducts]);
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-  return <div>carousel hi</div>;
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
 export default Carousel;
