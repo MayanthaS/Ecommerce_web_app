ab56fdcd6f6f142ac064715c7d10f90827cb926b
MayanthaS <sapumalmayantha@gmail.com>
2026-03-30 13:00:23 +0530

    feat: disable dots in Carousel component settings for cleaner UI

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -13,7 +13,7 @@ const Carousel = () => {
     fetchAllProducts();
   }, [fetchAllProducts]);
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
