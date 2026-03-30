c4efd163978a807bc7a82abe89e556e793253f01
MayanthaS <sapumalmayantha@gmail.com>
2026-03-30 15:37:05 +0530

    feat: render unique product categories as buttons in Category component

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -12,7 +12,20 @@ const Category = () => {
   };
   const uniqueCategories = getUniqueCategories(data, "category");
   console.log(uniqueCategories);
-  return <div></div>;
+  return (
+    <div className="bg-[#101829]">
+      <div className="max-w-7xl mx-auto flex gap-10 items-center justify-center py-7 px-4 ">
+        {uniqueCategories.map((category, index) => (
+          <button
+            key={index}
+            className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
