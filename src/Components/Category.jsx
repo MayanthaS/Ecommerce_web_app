1ffbf733e94d3f3b5fe07e54ef03b6e0282904fb
MayanthaS <sapumalmayantha@gmail.com>
2026-05-25 17:44:18 +0530

    feat: refactor Category component to improve category formatting and cleanup code

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -4,23 +4,30 @@ import { DataContext } from "../context/DataContext";
 const Category = () => {
   const { data } = useContext(DataContext);
 
+  const formatCategory = (category) => {
+    return category
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
   const getUniqueCategories = (products, property) => {
-    let newVAl = products?.map((curElem) => {
+    const newValue = products?.map((curElem) => {
       return curElem[property];
     });
-    return [...new Set(newVAl)];
+    return [...new Set(newValue)];
   };
   const uniqueCategories = getUniqueCategories(data, "category");
-  console.log(uniqueCategories);
+
   return (
     <div className="bg-[#140b63]">
-      <div className="max-w-8xl mx-auto flex gap-40 items-center justify-center py-5 px-6 ">
+      <div className="mx-auto flex max-w-6xl flex-wrap items-center justify-center gap-3 px-4 py-5 md:gap-20 lg:gap-40">
         {uniqueCategories.map((category, index) => (
           <button
             key={index}
             className="bg-blue-800 hover:bg-blue-800 text-white font-bold py-2 px-5 rounded cursor-pointer transition-colors duration-300"
           >
-            {category}
+            {formatCategory(category)}
           </button>
         ))}
       </div>
